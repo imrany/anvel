c04e44493d942e9f7527dc8106074afea5f5ede1
imrany <imranmat254@gmail.com>
2025-11-29 15:52:09 +0300

    chore: updated project name to Anvel

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([800.0, 600.0])
-            .with_title("File Explorer"),
+            .with_title("Anvel"),
         ..Default::default()
     };
 
     eframe::run_native(
-        "File Explorer",
+        "Anvel",
         options,
         Box::new(|_cc| Ok(Box::new(FileExplorer::default()))),
     )
